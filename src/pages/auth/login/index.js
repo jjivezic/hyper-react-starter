1c2938cdf8a00105d568a1f4b632c450aa80964c
jj_ivezic <ivezic.jj@gmail.com>
2021-09-19 22:46:52 +0200

    test

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -4,16 +4,16 @@ import { userActions } from 'store/actions/user.actions'
 
 const Login = () => {
   var [error, setError] = useState(false)
-  const dispatch = useDispatch()
+  var dispatch = useDispatch()
   const [userData, setUserData] = useState({
     email: '',
-    password: ''
+    password: '',
   })
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setError(false)
     const { name, value } = e.target
-    setUserData(userData => ({ ...userData, [name]: value }))
+    setUserData((userData) => ({ ...userData, [name]: value }))
   }
 
   const handleSubmit = async (e) => {
@@ -30,18 +30,38 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className='auth-form' style={{ textAlign: 'center' }}>
-      <h6 className='auth-title'>Login </h6>
+    <form onSubmit={handleSubmit} className="auth-form" style={{ textAlign: 'center' }}>
+      <h6 className="auth-title">Login </h6>
       <section>
-        <div className='form-group custom-input'>
-          <input id='email' className='form-control' type='text' name='email' onChange={handleChange} placeholder='email' />
+        <div className="form-group custom-input">
+          <input
+            id="email"
+            className="form-control"
+            type="text"
+            name="email"
+            onChange={handleChange}
+            placeholder="email"
+          />
         </div>
-        <div className='form-group custom-input'>
-          <input id='password' className='form-control' type='password' name='password' onChange={handleChange} placeholder='password' />
+        <div className="form-group custom-input">
+          <input
+            id="password"
+            className="form-control"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            placeholder="password"
+          />
         </div>
-        {error ? <div className='message-alert'><p>All fields are required</p></div> : null}
+        {error ? (
+          <div className="message-alert">
+            <p>All fields are required</p>
+          </div>
+        ) : null}
       </section>
-      <button type='submit' className='btn btn-blue'>SIgn in</button>
+      <button type="submit" className="btn btn-blue">
+        SIgn in
+      </button>
     </form>
   )
 }
