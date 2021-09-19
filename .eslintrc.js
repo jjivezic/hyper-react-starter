b7fe130a978b7f8a5c44e34ff325548e7dcc4f9d
jj_ivezic <ivezic.jj@gmail.com>
2021-09-20 00:46:47 +0200

    husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,17 @@ module.exports = {
     node: true
   },
   parser: 'babel-eslint',
-  extends: [
-    'standard', 'react-app'
-  ],
-  plugins: [
-    'babel'
-  ],
+  extends: ['standard', 'react-app', 'prettier'],
+  plugins: ['babel', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
     indent: ['error', 2, { SwitchCase: 1 }],
-    'react/prop-types': 0,
     'no-underscore-dangle': 0,
-    'no-plusplus': 0,
     'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
     'import/no-named-as-default': 0,
-    'import/no-named-as-default-member': 0
+    'import/no-named-as-default-member': 0,
+    semi: ['error', 'never'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 }
