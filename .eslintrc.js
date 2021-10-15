fc30f2883bddfb2ddc103f8c74ad0907f1c665ab
Jelena Ivezic <jj_ivezic@yahoo.com>
2021-10-15 15:42:43 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
   extends: ['standard', 'react-app', 'prettier'],
   plugins: ['babel', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
     indent: ['error', 2, { SwitchCase: 1 }],
     'no-underscore-dangle': 0,
     'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
