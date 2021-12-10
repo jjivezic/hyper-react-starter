c9a7585e3589fbe0fdfa84b1ff14161b31096f4d
hyper <nikola.josipovic@hyperether.com>
2021-12-10 12:55:23 +0100

    test pret

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -4,42 +4,38 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 
-const Dropdown = (props) => {
-    const [isDropdownOpen, setIsDropdownOpen]=useState(false)
-    const buttonClickHandler = () => {
-        if(window.innerWidth<992)
-            setIsDropdownOpen(!isDropdownOpen)
-    }
-    const mouseEnterHandler = () => {
-        if(window.innerWidth>=992)
-            setIsDropdownOpen(true)
-    }
-    const mouseLeaveHandler = () => {
-        if(window.innerWidth>=992)
-            setIsDropdownOpen(false)
-    }
-    return (
+const Dropdown = ({ label, options }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  const buttonClickHandler = () => {
+    if (window.innerWidth < 992) setIsDropdownOpen(!isDropdownOpen)
+  }
+  const mouseEnterHandler = () => {
+    if (window.innerWidth >= 992) setIsDropdownOpen(true)
+  }
+  const mouseLeaveHandler = () => {
+    if (window.innerWidth >= 992) setIsDropdownOpen(false)
+  }
+  return (
     <>
-        <div className="dropdown-container" onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
-                <button className="dropdown-title" onClick={buttonClickHandler}> 
-                    <span>{props.label}</span>
-                    <div className="dropdown-icon">
-                        {isDropdownOpen
-                            ?<FontAwesomeIcon icon={faAngleUp}/>
-                            :<FontAwesomeIcon icon={faAngleDown}/>}
-                    </div>
-                </button>
-                    <div className={isDropdownOpen?"dropdown-options active":"dropdown-options"}>
-                        {props.options.map((elem,index) => {
-                            return (
-                                <Link key={index} to={elem.path} className={elem.class}>{elem.title}</Link>
-                            )
-                        })
-                        }
-                    </div>
+      <div className="dropdown-container" onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
+        <button className="dropdown-title" onClick={buttonClickHandler}>
+          <span>{label}</span>
+          <div className="dropdown-icon">
+            {isDropdownOpen ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}
+          </div>
+        </button>
+        <div className={isDropdownOpen ? 'dropdown-options active' : 'dropdown-options'}>
+          {options.map((elem, index) => {
+            return (
+              <Link key={index} to={elem.path} className={elem.class}>
+                {elem.title}
+              </Link>
+            )
+          })}
         </div>
+      </div>
     </>
-    )
+  )
 }
 
 export default Dropdown
